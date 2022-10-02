5ca114e53d921d46e96db40f730df13390b9914d
Jacob Koziej <jacobkoziej@gmail.com>
2022-10-02 02:13:51 -0400

    Parse command line arguments fully

--- walker/walker.c
+++ walker/walker.c
@@ -16,9 +16,14 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <errno.h>
+#include <limits.h>
+#include <pwd.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 
@@ -28,14 +33,26 @@
 #define VOL_CHK (1 << 3)
 
 
+static uint_fast8_t flags;
+
+static const char *path = ".";
+static dev_t       sl_dev;
+static ino_t       sl_ino;
+static dev_t       vol;
+static uid_t       uid;
+static long        mtime;
+
+static struct stat st;
+
+
 int main(int argc, char **argv)
 {
-	static uint_fast8_t flags;
-
-	static const char *path = ".";
-
 	{
 		int opt;
+		const char *strlink  = NULL;
+		const char *strmtime = NULL;
+		const char *struser  = NULL;
+
 		opterr = 0;
 		while ((opt = getopt(argc, argv, ":hl:m:u:x")) != -1) {
 			switch (opt) {
@@ -47,15 +64,18 @@ int main(int argc, char **argv)
 				return 0;
 
 			case 'l':
-				flags |= LNK_CHK;
+				flags   |= LNK_CHK;
+				strlink  = optarg;
 				break;
 
 			case 'm':
-				flags |= MTM_CHK;
+				flags    |= MTM_CHK;
+				strmtime  = optarg;
 				break;
 
 			case 'u':
-				flags |= UID_CHK;
+				flags   |= UID_CHK;
+				struser  = optarg;
 				break;
 
 			case 'x':
@@ -92,9 +112,72 @@ int main(int argc, char **argv)
 				return EXIT_FAILURE;
 			}
 		}
-	}
 
-	if (argc - optind) path = argv[argc - 1];
+		if (argc - optind) path = argv[argc - 1];
+
+		if (flags & LNK_CHK) {
+			if (lstat(strlink, &st) < 0) {
+				perror("failed to get link target file status");
+				return EXIT_FAILURE;
+			}
+			sl_dev = st.st_dev;
+			sl_ino = st.st_ino;
+		}
+
+		if (flags & MTM_CHK) {
+			char *invalid = NULL;
+			mtime = strtoul(strmtime, &invalid, 10);
+
+			if (*strmtime && *invalid) {
+				fprintf(
+					stderr,
+					"invalid mtime: %s\n",
+					strmtime
+				);
+				return EXIT_FAILURE;
+			}
+		}
+
+		if (flags & UID_CHK) {
+			char *invalid = NULL;
+			uid = strtoul(struser, &invalid, 10);
+
+			uid &= UINT_MAX;  // mask invalid UID
+
+			if (*struser && *invalid) {
+				struct passwd *user = getpwnam(struser);
+				if (!user) {
+					if (
+						!errno            ||
+						(errno == ENOENT) ||
+						(errno == ESRCH)  ||
+						(errno == EBADF)  ||
+						(errno == EPERM)
+					) {
+						fprintf(
+							stderr,
+							"no such user: %s\n",
+							struser
+						);
+					} else {
+						perror("getpwnam() failed");
+					}
+
+					return EXIT_FAILURE;
+				}
+
+				uid = user->pw_uid;
+			}
+		}
+
+		if (flags & VOL_CHK) {
+			if (lstat(path, &st) < 0) {
+				perror("failed to get volume information");
+				return EXIT_FAILURE;
+			}
+			vol = st.st_dev;
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
