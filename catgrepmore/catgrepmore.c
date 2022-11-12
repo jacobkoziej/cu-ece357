4f185f3fb8146b6e4acca7f53bdff22083fd24e9
Jacob Koziej <jacobkoziej@gmail.com>
2022-11-11 23:50:26 -0500

    Define `catgrepmore()`

--- catgrepmore/catgrepmore.c
+++ catgrepmore/catgrepmore.c
@@ -104,6 +104,148 @@ write_eintr:
 	}
 }
 
+static void catgrepmore(const char *pattern, const char *path)
+{
+	int fd;
+	int pipefd[PIPE_CNT][2];
+
+open_eintr:
+	fd = open(path, O_RDONLY);
+	if (fd < 0) {
+		if (errno == EINTR) goto open_eintr;
+
+		perror(path);
+		exit(EXIT_FAILURE);
+	}
+
+	if (pipe(pipefd[GREP_PIPE]) < 0) {
+		perror("pipe(pipefd[GREP_PIPE]):");
+		exit(EXIT_FAILURE);
+	}
+
+	if (pipe(pipefd[MORE_PIPE]) < 0) {
+		perror("pipe(pipefd[MORE_PIPE]):");
+		exit(EXIT_FAILURE);
+	}
+
+	switch (sigsetjmp(jmp, 1)) {
+	case SIGUSR2:
+		fprintf(
+			stderr,
+			"SIGUSR2 recieved, moving on from file #%lu\n",
+			files
+		);
+		goto sigjmp;
+		break;
+
+	case SIGPIPE:
+		fprintf(
+			stderr,
+			"SIGPIPE recieved, moving on from file #%lu\n",
+			files
+		);
+		goto sigjmp;
+		break;
+	}
+
+	sig.sa_handler = next_file_handler;
+
+	if (sigaction(SIGUSR2, &sig, NULL) < 0) {
+		perror("failed to setup signal handler for SIGUSR1");
+		exit(EXIT_FAILURE);
+	}
+	if (sigaction(SIGPIPE, &sig, NULL) < 0) {
+		perror("failed to setup signal handler for SIGPIPE");
+		exit(EXIT_FAILURE);
+	}
+
+	pid_t grep_pid;
+	pid_t more_pid;
+
+	switch (grep_pid = fork()) {
+	case -1:
+		goto grep_child_error;
+
+	case 0:
+		if (dup2(pipefd[GREP_PIPE][0], STDIN_FILENO) < 0)
+			goto grep_child_error;
+
+		if (dup2(pipefd[MORE_PIPE][1], STDOUT_FILENO) < 0)
+			goto grep_child_error;
+
+		if (close(fd) < 0) goto grep_child_error;
+		for (int i = 0; i < PIPE_CNT; i++)
+			for (int j = 0; j < 2; j++)
+				if (close(pipefd[i][j]) < 0)
+					goto grep_child_error;
+
+		execlp("grep", "grep", pattern, NULL);
+
+grep_child_error:
+		perror("failed to fork grep child");
+		exit(EXIT_FAILURE);
+	}
+
+	switch (more_pid = fork()) {
+	case -1:
+		goto more_child_error;
+
+	case 0:
+		if (dup2(pipefd[MORE_PIPE][0], STDIN_FILENO) < 0)
+			goto more_child_error;
+
+		if (close(fd) < 0) goto more_child_error;
+		for (int i = 0; i < PIPE_CNT; i++)
+			for (int j = 0; j < 2; j++)
+				if (close(pipefd[i][j]) < 0)
+					goto more_child_error;
+
+		execlp("more", "more", NULL);
+
+more_child_error:
+		perror("failed to fork more child");
+		exit(EXIT_FAILURE);
+	}
+
+	writer(fd, pipefd[GREP_PIPE][1]);
+
+sigjmp:
+	for (int i = 0; i < PIPE_CNT; i++)
+		for (int j = 0; j < 2; j++)
+pipe_close_eintr:
+			if (close(pipefd[i][j]) < 0) {
+				if (errno == EINTR)
+					goto pipe_close_eintr;
+
+				perror("failed to close pipes");
+				exit(EXIT_FAILURE);
+			}
+
+	wait(NULL);
+	wait(NULL);
+
+	sig.sa_handler = SIG_DFL;
+
+	if (sigaction(SIGUSR2, &sig, NULL) < 0) {
+		perror("failed to reset SIGUSR2 signal");
+		exit(EXIT_FAILURE);
+	}
+	if (sigaction(SIGPIPE, &sig, NULL) < 0) {
+		perror("failed to reset SIGPIPE signal");
+		exit(EXIT_FAILURE);
+	}
+
+close_eintr:
+	if (close(fd) < 0) {
+		if (errno == EINTR) goto close_eintr;
+
+		perror("close()");
+		exit(EXIT_FAILURE);
+	}
+
+	++files;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -122,146 +264,8 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	for (int i = 2; i < argc; i++) {
-		int fd;
-		int pipefd[PIPE_CNT][2];
-
-open_eintr:
-		fd = open(argv[i], O_RDONLY);
-		if (fd < 0) {
-			if (errno == EINTR) goto open_eintr;
-
-			perror(argv[i]);
-			return EXIT_FAILURE;
-		}
-
-		if (pipe(pipefd[GREP_PIPE]) < 0) {
-			perror("pipe(pipefd[GREP_PIPE]):");
-			return EXIT_FAILURE;
-		}
-
-		if (pipe(pipefd[MORE_PIPE]) < 0) {
-			perror("pipe(pipefd[MORE_PIPE]):");
-			return EXIT_FAILURE;
-		}
-
-		switch (sigsetjmp(jmp, 1)) {
-		case SIGUSR2:
-			fprintf(
-				stderr,
-				"SIGUSR2 recieved, moving on from file #%lu\n",
-				files
-			);
-			goto sigjmp;
-			break;
-
-		case SIGPIPE:
-			fprintf(
-				stderr,
-				"SIGPIPE recieved, moving on from file #%lu\n",
-				files
-			);
-			goto sigjmp;
-			break;
-		}
-
-		sig.sa_handler = next_file_handler;
-
-		if (sigaction(SIGUSR2, &sig, NULL) < 0) {
-			perror("failed to setup signal handler for SIGUSR1");
-			return EXIT_FAILURE;
-		}
-		if (sigaction(SIGPIPE, &sig, NULL) < 0) {
-			perror("failed to setup signal handler for SIGPIPE");
-			return EXIT_FAILURE;
-		}
-
-		pid_t grep_pid;
-		pid_t more_pid;
-
-		switch (grep_pid = fork()) {
-		case -1:
-			goto grep_child_error;
-
-		case 0:
-			if (dup2(pipefd[GREP_PIPE][0], STDIN_FILENO) < 0)
-				goto grep_child_error;
-
-			if (dup2(pipefd[MORE_PIPE][1], STDOUT_FILENO) < 0)
-				goto grep_child_error;
-
-			if (close(fd) < 0) goto grep_child_error;
-			for (int i = 0; i < PIPE_CNT; i++)
-				for (int j = 0; j < 2; j++)
-					if (close(pipefd[i][j]) < 0)
-						goto grep_child_error;
-
-			execlp("grep", "grep", argv[1], NULL);
-
-grep_child_error:
-			perror("failed to fork grep child");
-			return EXIT_FAILURE;
-		}
-
-		switch (more_pid = fork()) {
-		case -1:
-			goto more_child_error;
-
-		case 0:
-			if (dup2(pipefd[MORE_PIPE][0], STDIN_FILENO) < 0)
-				goto more_child_error;
-
-			if (close(fd) < 0) goto more_child_error;
-			for (int i = 0; i < PIPE_CNT; i++)
-				for (int j = 0; j < 2; j++)
-					if (close(pipefd[i][j]) < 0)
-						goto more_child_error;
-
-			execlp("more", "more", NULL);
-
-more_child_error:
-			perror("failed to fork more child");
-			return EXIT_FAILURE;
-		}
-
-		writer(fd, pipefd[GREP_PIPE][1]);
-
-sigjmp:
-		for (int i = 0; i < PIPE_CNT; i++)
-			for (int j = 0; j < 2; j++)
-pipe_close_eintr:
-				if (close(pipefd[i][j]) < 0) {
-					if (errno == EINTR)
-						goto pipe_close_eintr;
-
-					perror("failed to close pipes");
-					return EXIT_FAILURE;
-				}
-
-		wait(NULL);
-		wait(NULL);
-
-		sig.sa_handler = SIG_DFL;
-
-		if (sigaction(SIGUSR2, &sig, NULL) < 0) {
-			perror("failed to reset SIGUSR2 signal");
-			return EXIT_FAILURE;
-		}
-		if (sigaction(SIGPIPE, &sig, NULL) < 0) {
-			perror("failed to reset SIGPIPE signal");
-			return EXIT_FAILURE;
-		}
-
-close_eintr:
-		if (close(fd) < 0) {
-			if (errno == EINTR) goto close_eintr;
-
-			perror("close()");
-			return EXIT_FAILURE;
-		}
-
-		++files;
-	}
+	for (int i = 2; i < argc; i++)
+		catgrepmore(argv[1], argv[i]);
 
 	return EXIT_SUCCESS;
 }
