4898fa3b3ab7e79d78aff02d60cdb8a1543ae938
Jacob Koziej <jacobkoziej@gmail.com>
2022-12-02 15:43:41 -0500

    Parse command line arguments

--- bgrep/bgrep.c
+++ bgrep/bgrep.c
@@ -16,10 +16,85 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 
-int main(void)
+int main(int argc, char **argv)
 {
-	return EXIT_SUCCESS;
+	static size_t       context;
+	static int          fd;
+	static char        *pattern;
+	static size_t       pattern_len;
+	static struct stat  sb;
+
+	int opt;
+	while ((opt = getopt(argc, argv, "c:p:")) != -1) {
+		switch (opt) {
+			case 'c':
+				context = strtoul(optarg, NULL, 0);
+				break;
+
+			case 'p':
+				// we already have a pattern
+				if (pattern) {
+					perror("pattern already specified");
+					return -1;
+				}
+
+				fd = open(optarg, O_RDONLY);
+				if (fd < 0) {
+					perror(optarg);
+					return -1;
+				}
+
+				if (fstat(fd, &sb) < 0) {
+					perror(optarg);
+					return -1;
+				}
+
+				pattern_len = sb.st_size;
+				pattern = mmap(
+					NULL,
+					pattern_len,
+					PROT_READ,
+					MAP_PRIVATE,
+					fd,
+					0
+				);
+
+				if (pattern == MAP_FAILED) {
+					perror(optarg);
+					return -1;
+				}
+
+				if (close(fd) < 0) {
+					perror(optarg);
+					return -1;
+				}
+
+				break;
+
+			default:
+				return -1;
+		}
+	}
+
+	if (!pattern) {
+		if (argc <= optind) {
+			fprintf(stderr, "%s: no pattern specified\n", *argv);
+			return -1;
+		}
+
+		pattern     = argv[optind++];
+		pattern_len = strlen(pattern);
+	}
+
+	return 0;
 }
