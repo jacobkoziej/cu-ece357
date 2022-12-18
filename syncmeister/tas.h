248d130169e697f2bd8d08fe2d10d80ba461bc6c
Jacob Koziej <jacobkoziej@gmail.com>
2022-12-17 19:13:42 -0500

    Add `tas.h`

---
+++ syncmeister/tas.h
@@ -0,0 +1,26 @@
+/*
+ * tas.h -- test-and-set
+ * Copyright (C) 2022  Jacob Koziej <jacobkoziej@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef TAS_H
+#define TAS_H
+
+
+int tas(volatile char *lock);
+
+
+#endif /* TAS_H */
