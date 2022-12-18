471c8845694c0fc890b84d387d282d552228f815
Jacob Koziej <jacobkoziej@gmail.com>
2022-12-18 02:03:50 -0500

    Add `fifo.h`

---
+++ syncmeister/fifo.h
@@ -0,0 +1,47 @@
+/*
+ * fifo.h -- primitive FIFO
+ * Copyright (C) 2022  Jacob Koziej <jacobkoziej@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef FIFO_H
+#define FIFO_H
+
+
+#include <stddef.h>
+
+#include "cv.h"
+#include "spinlock.h"
+
+
+#define FIFO_BUFSIZ (1 << 10)
+
+
+struct fifo {
+	size_t head;
+	size_t tail;
+	size_t use;
+	struct cv cv;
+	struct spinlock mutex;
+	unsigned long fifo[FIFO_BUFSIZ];
+};
+
+
+void          fifo_init(struct fifo *fifo);
+unsigned long fifo_rd(struct fifo *fifo);
+void          fifo_wr(struct fifo *fifo, unsigned long val);
+
+
+#endif /* FIFO_H */
